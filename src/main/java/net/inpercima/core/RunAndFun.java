426baf092a48f8ec63ea156e0b163bfbca9fa0cb
Sebastian Peters <Sebastian.Peters@gmail.com>
2015-01-30 22:03:18 +0100

    add .gitignore, gradle support and spring-boot

---
+++ src/main/java/net/inpercima/core/RunAndFun.java
@@ -0,0 +1,17 @@
+package net.inpercima.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class RunAndFun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunAndFun.class, args);
+    }
+
+}
