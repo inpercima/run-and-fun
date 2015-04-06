ffd0799f319978b89795f07fccfbdd47f2424423
Sebastian Peters <Sebastian.Peters@gmail.com>
2015-04-06 20:06:20 +0200

    react on runkeeper authorization denied
    * log as warn
    * redirect to home

--- src/main/java/net/inpercima/runandfun/web/ViewController.java
+++ src/main/java/net/inpercima/runandfun/web/ViewController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpSession;
 import net.inpercima.runandfun.constants.AppConstants;
 import net.inpercima.runandfun.service.RunAndFunService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +21,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ViewController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ViewController.class);
+
     private static final String REDIRECT_TO_BASE = "redirect:/";
+
+    private static final String ACCESS_DENIED = "access_denied";
 
     @Autowired
     private RunAndFunService runAndFunService;
 
     @RequestMapping(value = "/verify")
-    public String verify(final HttpSession session, @RequestParam(value = "code", required = true) final String code) {
-        session.setAttribute(SESSION_ACCESS_TOKEN, runAndFunService.getAccessToken(code));
+    public String verify(final HttpSession session, @RequestParam(value = "code", required = false) final String code,
+            @RequestParam(value = "error", required = false) final String error) {
+        if (!ACCESS_DENIED.equals(error)) {
+            session.setAttribute(SESSION_ACCESS_TOKEN, runAndFunService.getAccessToken(code));
+        } else {
+            LOGGER.warn(error);
+        }
         return REDIRECT_TO_BASE;
     }
 
