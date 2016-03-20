a80abf7d539581f458205b7ce3767cc257a8406d
inpercima <inpercima@gmail.com>
2016-03-20 20:07:44 +0100

    fix the double entries all years in selectbox
    * reorder filterType to pass filterYears
    * use 'all' instead off empty ('') for all years

--- src/main/resources/public/js/graphs.controller.js
+++ src/main/resources/public/js/graphs.controller.js
@@ -47,7 +47,7 @@
 
       var minDate = $filter(DATE)(vm.filterMinDate, DATE_PATTERN);
       var maxDate = $filter(DATE)(vm.filterMaxDate, DATE_PATTERN);
-      if (vm.filterYear.key) {
+      if (vm.filterYear.key !== 'all') {
         minDate = vm.filterYear.year + '-01-01';
         maxDate = vm.filterYear.year + '-12-31';
       }
@@ -181,7 +181,7 @@
     }
 
     function years() {
-      var filterAll = simpleKeyYear('');
+      var filterAll = simpleKeyYear('all');
       vm.years.push(filterAll);
       var startYear = 2010;
       var endYear = new Date().getFullYear();
@@ -194,7 +194,7 @@
     function simpleKeyYear(key) {
       return {
         'key': key,
-        'year': key === '' ? 'All years' : key
+        'year': key === 'all' ? 'All years' : key
       };
     }
   }
