inpercima/run-and-fun

commit a80abf7d539581f458205b7ce3767cc257a8406d

fix the double entries all years in selectbox
* reorder filterType to pass filterYears
* use 'all' instead off empty ('') for all years

src/main/resources/public/js/graphs.controller.js
@@ -47,7 +47,7 @@
 
       var minDate = $filter(DATE)(vm.filterMinDate, DATE_PATTERN);
       var maxDate = $filter(DATE)(vm.filterMaxDate, DATE_PATTERN);
-      if (vm.filterYear.key) {
+      if (vm.filterYear.key !== 'all') {
         minDate = vm.filterYear.year + '-01-01';
         maxDate = vm.filterYear.year + '-12-31';
       }
@@ -181,7 +181,7 @@
     }
 
     function years() {
-      var filterAll = simpleKeyYear('');
+      var filterAll = simpleKeyYear('all');
       vm.years.push(filterAll);
       var startYear = 2010;
       var endYear = new Date().getFullYear();
@@ -194,7 +194,7 @@
     function simpleKeyYear(key) {
       return {
         'key': key,
-        'year': key === '' ? 'All years' : key
+        'year': key === 'all' ? 'All years' : key
       };
     }
   }

src/main/resources/public/js/activity.controller.js
@@ -43,7 +43,7 @@
       loginService.state(vm);
       var minDate = $filter('date')(vm.filterMinDate, 'yyyy-MM-dd');
       var maxDate = $filter('date')(vm.filterMaxDate, 'yyyy-MM-dd');
-      if (vm.filterYear.key) {
+      if (vm.filterYear.key !== 'all') {
         minDate = vm.filterYear.year + '-01-01';
         maxDate = vm.filterYear.year + '-12-31';
       }
@@ -78,7 +78,7 @@
     }
 
     function years() {
-      var filterAll = simpleKeyYear('');
+      var filterAll = simpleKeyYear('all');
       vm.years.push(filterAll);
       var startYear = 2010;
       var endYear = new Date().getFullYear();
@@ -100,7 +100,7 @@
     function simpleKeyYear(key) {
       return {
         'key': key,
-        'year': key === '' ? 'All years' : key
+        'year': key === 'all' ? 'All years' : key
       };
     }
 

src/main/resources/public/partials/activities.html
@@ -8,7 +8,7 @@
     <input class="form-control" type="number" data-ng-change="vm.list()" data-ng-model="vm.size" placeholder="Resultset size" id="size" min="0" />
   </div>
   <div class="form-group form-inline">
-    <select class="form-control" data-ng-disabled="vm.allActivityTypes" data-ng-change="vm.list()" data-ng-model="vm.filterType" id="filterType" data-ng-options="filterType.type for filterType in vm.types track by filterType.key" multiple></select>
+    <select class="form-control" data-ng-change="vm.list()" data-ng-model="vm.filterType" id="filterType" data-ng-options="filterType.type for filterType in vm.types track by filterType.key" data-ng-disabled="vm.allActivityTypes" multiple></select>
     <input id="allActivityTypes" type="checkbox" data-ng-model="vm.allActivityTypes" data-ng-change="vm.list()" />
     <label for="allActivityTypes">All activity types</label>
   </div>

commit 2c286b8185f02e3628d10f046dfa23f824fe05a3

fix multiple series for monthly charts of all years
* bug: multiple series are missing in monthly chart
* bug introduced with a80abf7 b/c key is now 'all' instead of ''
* define const KEY_ALL
* change condition for multipleSeries

src/main/resources/public/js/graphs.controller.js
@@ -20,6 +20,8 @@
     const KM_PER_MONTH = 'month';
     const KM_PER_DAY = 'day';
 
+    const KEY_ALL = 'all';
+
     // public methods
     vm.list = list;
     vm.refreshLineKmPerMonth = refreshLineKmPerMonth;
@@ -47,7 +49,7 @@
 
       var minDate = $filter(DATE)(vm.filterMinDate, DATE_PATTERN);
       var maxDate = $filter(DATE)(vm.filterMaxDate, DATE_PATTERN);
-      if (vm.filterYear.key !== 'all') {
+      if (vm.filterYear.key !== KEY_ALL) {
         minDate = vm.filterYear.year + '-01-01';
         maxDate = vm.filterYear.year + '-12-31';
       }
@@ -85,7 +87,7 @@
       var data = {};
       var labels = {};
 
-      var multipleSeries = !vm.filterYear.key && groupBy === KM_PER_MONTH;
+      var multipleSeries = vm.filterYear.key === KEY_ALL && groupBy === KM_PER_MONTH;
       if (multipleSeries) {
         for (var month = 1; month <= 12; month++) {
           vm.lineKmPerMonthLabels.push((month < 10 ? '0' : '') + month);
@@ -181,7 +183,7 @@
     }
 
     function years() {
-      var filterAll = simpleKeyYear('all');
+      var filterAll = simpleKeyYear(KEY_ALL);
       vm.years.push(filterAll);
       var startYear = 2010;
       var endYear = new Date().getFullYear();
@@ -194,7 +196,7 @@
     function simpleKeyYear(key) {
       return {
         'key': key,
-        'year': key === 'all' ? 'All years' : key
+        'year': key === KEY_ALL ? 'All years' : key
       };
     }
   }