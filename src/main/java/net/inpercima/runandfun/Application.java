b35358c83fc6aa9bee5ceebb95df6b358513a6da
Sebastian Peters <Sebastian.Peters@gmail.com>
2016-10-30 13:40:56 +0100

    Upgrade spring-boot to 1.4.1
    Add mandatory runtime dependency to jna
    see elastic/elasticsearch#19045 and elastic/elasticsearch#13245
    
    Use servlet.ErrorPage instead of deprecated embedded.ErrorPage
    see http://docs.spring.io/spring-boot/docs/current/api/org/springframework/boot/context/embedded/ErrorPage.html

--- src/main/java/net/inpercima/runandfun/Application.java
+++ src/main/java/net/inpercima/runandfun/Application.java
@@ -3,7 +3,7 @@ package net.inpercima.runandfun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
