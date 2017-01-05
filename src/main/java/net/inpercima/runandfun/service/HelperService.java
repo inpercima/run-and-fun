971369d4c0e4aae24c598415d1e0ec5024096968
inpercima <inpercima@gmail.com>
2017-01-05 20:39:03 +0100

    Resorting constants and methods Implement access to root resource Refactoring constants

--- src/main/java/net/inpercima/runandfun/service/HelperService.java
+++ src/main/java/net/inpercima/runandfun/service/HelperService.java
@@ -10,11 +10,17 @@ public interface HelperService {
 
     String getClientId();
 
+    void setClientId(final String clientId);
+
+    void setClientSecret(final String clientSecret);
+
     String getRedirectUri();
+
+    void postForObject(String url, String accessToken);
 
     <T> T postForObject(String url, String code, Class<T> clazz);
 
-    void postForObject(String url, String accessToken);
+    <T> HttpEntity<T> getForObject(String url, String applicationType, Class<T> clazz);
 
     <T> HttpEntity<T> getForObject(String url, String applicationType, String accessToken, Class<T> clazz);
 
