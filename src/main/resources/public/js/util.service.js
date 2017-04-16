a183bdcfb12d2189c8e472a170598003a86290bb
inpercima <inpercima@gmail.com>
2017-04-16 19:28:15 +0200

    Refactor package.json and util.service.js

--- src/main/resources/public/js/util.service.js
+++ src/main/resources/public/js/util.service.js
@@ -12,25 +12,19 @@
     const YEAR_BEGIN = '-01-01';
     const YEAR_END = '-12-31';
 
-    service.simpleKeyYear = function(key) {
-      return {
-        key,
-        year: key === CONST.KEY_ALL ? LABEL_ALL_YEARS : key,
-      };
-    };
+    service.simpleKeyYear = (key) => ({
+      key,
+      year: key === CONST.KEY_ALL ? LABEL_ALL_YEARS : key,
+    });
 
-    service.simpleKeyType = function(key) {
-      return {
-        key,
-        type: key,
-      };
-    };
+    service.simpleKeyType = (key) => ({
+      key,
+      type: key,
+    });
 
-    service.dateFilter = function(value) {
-      return $filter(CONST.DATE)(value, CONST.DATE_PATTERN);
-    };
+    service.dateFilter = (value) => $filter(CONST.DATE)(value, CONST.DATE_PATTERN);
 
-    service.listYears = function(firstEntry) {
+    service.listYears = (firstEntry) => {
       const result = [firstEntry];
       for (let year = 2010; year <= new Date().getFullYear(); year++) {
         result.push(service.simpleKeyYear(year));
@@ -38,18 +32,14 @@
       return result;
     };
 
-    service.getMinMaxDate = function(filterYear, filterMinDate, filterMaxDate) {
-      return {
-        minDate: filterYear.key !== CONST.KEY_ALL ? filterYear.year + YEAR_BEGIN : service.dateFilter(filterMinDate),
-        maxDate: filterYear.key !== CONST.KEY_ALL ? filterYear.year + YEAR_END : service.dateFilter(filterMaxDate),
-      };
-    };
+    service.getMinMaxDate = (filterYear, filterMinDate, filterMaxDate) => ({
+      minDate: filterYear.key !== CONST.KEY_ALL ? filterYear.year + YEAR_BEGIN : service.dateFilter(filterMinDate),
+      maxDate: filterYear.key !== CONST.KEY_ALL ? filterYear.year + YEAR_END : service.dateFilter(filterMaxDate),
+    });
 
-    service.getCurrentPage = function() {
-      return {
-        name: $route.current.name,
-        subtext: $route.current.text,
-      };
-    };
+    service.getCurrentPage = () => ({
+      name: $route.current.name,
+      subtext: $route.current.text,
+    });
   }
 })();
